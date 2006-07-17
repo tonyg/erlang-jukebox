dfee9459369044f46e779625fadad2f65dca8ba3
Tony Garnock-Jones <tonyg@lshift.net>
2006-07-17 17:14:45 +0000

    Call out to ogg123 with "-d oss".

--- player.erl
+++ player.erl
@@ -23,7 +23,7 @@ supports_extension(Extension) ->
 
 player_mapping(E) -> player_mapping1(http_util:to_lower(E)).
 
-player_mapping1(".ogg") -> {ok, ["/usr/bin/env", "ogg123", "-q", url]};
+player_mapping1(".ogg") -> {ok, ["/usr/bin/env", "ogg123", "-d", "oss", "-q", url]};
 player_mapping1(".mp3") -> {ok, ["/usr/bin/env", "mpg123", "-q", url]};
 player_mapping1(_) -> not_playable.
 
