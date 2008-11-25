07e79b41af0a76394b2130746a840dc4c9adccc4
Simon MacMullen <simon@lshift.net>
2008-11-25 15:15:33 +0000

    Forgot to remove
    --HG--
    branch : metadata

--- src/jukeboxsession.erl
+++ src/jukeboxsession.erl
@@ -2,7 +2,6 @@
 -behaviour(gen_server).
 
 -include("tqueue.hrl").
--include("info.hrl").
 
 -export([start_link/0]).
 -export([init/1, terminate/2, code_change/3, handle_call/3, handle_cast/2, handle_info/2]).
